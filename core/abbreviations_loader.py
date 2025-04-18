92e22ce3705a308baea06137a0b38c0de6d2320e
raphael <rafavfx1@gmail.com>
2025-04-18 09:39:40 -0300

    Fix load_abbreviations function to reference the correct abrev.json file and add initial abbreviations data

--- core/abbreviations_loader.py
+++ core/abbreviations_loader.py
@@ -1,5 +1,5 @@
 import json
 
 def load_abbreviations():
-    with open('data/exemplos.json', 'r', encoding='utf-8') as f:
+    with open('data/abrev.json', 'r', encoding='utf-8') as f:
         return json.load(f)
