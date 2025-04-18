5d345010fcd13722654894cb04aadb389fbd73b0
raphael <rafavfx1@gmail.com>
2025-04-18 10:59:04 -0300

    Remove NLTK dependencies and streamline abbreviation expansion logic; update abbreviation mappings in JSON file.

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-import nltk
-
-nltk.download('punkt', quiet=True) # Tokenizador de palavras e sentenças
-nltk.download('stopwords', quiet=True) # Lista de stopwords (palavras comuns) em vários idiomas
-nltk.download('rslp', quiet=True)  # Stemmer para o português (RSLP)
--- core/abbreviation_expander.py
+++ core/abbreviation_expander.py
@@ -1,30 +1,31 @@
 import re
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 
 def preprocess_and_expand(text, abbreviation_dict):
+    text = re.sub(r'https?://\S+|www\.\S+', '', text) # Remove URLs
     
-    tokens = word_tokenize(text, language='portuguese') # Tokeniza o texto
-    stop_words = set(stopwords.words('portuguese'))
+    emoji_pattern = re.compile("["
+        u"\U0001F600-\U0001F64F"  # Emojis emotivos
+        u"\U0001F300-\U0001F5FF"  # Símbolos e pictogramas
+        u"\U0001F680-\U0001F6FF"  # Transporte e mapas
+        u"\U0001F1E0-\U0001F1FF"  # Bandeiras
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+", flags=re.UNICODE)
+    text = emoji_pattern.sub(r'', text)
 
-    processed_tokens = []
-
-    for token in tokens:
-        token_lower = token.lower()
-
+    
+    for abbrev, expansion in abbreviation_dict.items(): # Expande abreviações baseando se no dicionario criado, que está no data/abrev.json
+        pattern = re.compile(rf'\b{re.escape(abbrev)}\b', flags=re.IGNORECASE)
         
-        if token_lower in abbreviation_dict: # Expande abreviações
-            expansion = abbreviation_dict[token_lower]
+        def replace(match):
+            word = match.group(0)
+            if word.isupper():
+                return expansion.upper()
+            elif word[0].isupper():
+                return expansion.capitalize()
+            else:
+                return expansion.lower()
+        
+        text = pattern.sub(replace, text)
 
-            if token.istitle():
-                expansion = expansion.capitalize()
-            elif token.isupper():
-                expansion = expansion.upper()
-
-            processed_tokens.append(expansion)
-        else:  # Mantém a palavra se não for uma stopword ou for pontuação
-            if token_lower not in stop_words or not token.isalpha():
-                processed_tokens.append(token)
-
-    return ' '.join(processed_tokens)
+    return text.strip()
